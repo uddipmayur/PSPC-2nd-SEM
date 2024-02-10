f6815941abd09faf41941af283a124a4c51d78da
UddipMayur <94662823+uddipmayur@users.noreply.github.com>
2024-02-10 22:12:38 +0530

    Update Odd Even with Void Function.c

--- Odd Even with Void Function.c
+++ Odd Even with Void Function.c
@@ -6,17 +6,15 @@ void oddeven(int n)
     {
         printf("You entered 0.\n");
     }
+    else if(n % 2 == 0)
+    {
+        printf("The number is even.\n");
+    }
     else
     {
-        if(n % 2 == 0)
-        {
-            printf("The number is even.\n");
-        }
-        else
-        {
-            printf("The number is odd.\n");
-        }
+        printf("The number is odd.\n");
     }
+
     return 0;
 }
 
