e4db088b3635badaa00b94855f4ad98af25d4143
UddipMayur <94662823+uddipmayur@users.noreply.github.com>
2024-02-16 21:22:29 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int fact(int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
+
+
+int main()
+{
+    int num, result = 0;
+
+    printf("Enter a number to find its factorial: ");
+    scanf("%d", &num);
+
+    if(num == 0)
+    {
+        printf("The Factorial of 0 is 0");
+    }
+    else
+    {
+        result = fact(num);
+    }
+
+    printf("The Factorial of %d is %d", num, result);
+
+    return 0;
+}
