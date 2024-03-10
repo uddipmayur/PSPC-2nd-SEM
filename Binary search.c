06e6122837414e40003dcb05d1024c2c9ea17f08
uddipmayur <uddipkrishnadas2@gmail.com>
2024-03-11 03:34:38 +0530

    Updated

--- Binary search.c
+++ Binary search.c
@@ -9,13 +9,13 @@ int binary_search(int arr[], int low, int high, int target)
     {
         int mid = low + (high -low) / 2;
         if(arr[mid] == target)
-    {
-    return mid;
-    }
-    if(arr[mid] < target)
-    {
-        low = mid + 1;
-    }
+        {
+            return mid;
+        }
+        if(arr[mid] < target)
+        {
+            low = mid + 1;
+        }
     else
     {
         high = mid - 1;
