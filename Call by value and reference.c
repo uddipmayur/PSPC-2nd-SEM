8826dba84065dc4f7f00a70904aaa760172b384c
uddipmayur <uddipkrishnadas2@gmail.com>
2024-03-10 20:52:56 +0530

    Update

---
+++ Call by value and reference.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+void call_by_value(int x)
+{
+    x+= 20;
+    printf("\nNumber inside call by value: %d", x);
+}
+
+
+
+void call_by_reference(int *x)
+{
+    *x+= 20;
+    printf("\nNumber inside call by reference: %d", *x);
+}
+
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Number before call by value: %d", num);
+    call_by_value(num);
+    printf("\nNumber after call by value: %d", num);
+
+
+    printf("\nNumber before call by value: %d", num);
+    call_by_reference(&num);
+    printf("\nNumber after call by value: %d", num);
+
+
+    return 0;
+}
