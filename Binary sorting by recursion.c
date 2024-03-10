88ad0a4590769df73612199180e5971e58be6ee9
UddipMayur <94662823+uddipmayur@users.noreply.github.com>
2024-03-10 20:47:23 +0530

    Update Binary sorting by recursion.c

--- Binary sorting by recursion.c
+++ Binary sorting by recursion.c
@@ -1,42 +1,49 @@
 //Binary sorting by recursion
 
 
-int binary_search(int arr, int low, int high, int target)
+#include<stdio.h>
+
+
+int binary_search(int arr[], int low, int high, int target)
 {
- if(low <= high)
- {
-  int mid = low + (high - low) / 2;
-  if arr[mid] == x)
-  {
-   return mid;
-  }
-  if(arr[mid] < target)
-  {
-   return binary_search(ar, mid + 1, high, target)
-  }
-  else
-  {
-   return binary_search(arr, low, mid - 1, target)
-  }
- }
+    if(low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if(arr[mid] == target)
+        {
+            return mid;
+        }
+        if(arr[mid] < target)
+        {
+        return binary_search(arr, mid + 1, high, target);
+        }
+        else
+        {
+            return binary_search(arr, low, mid - 1, target);
+        }
+    }
 }  
 
 
 int main()
 {
- arr[] = {1,3,5,6,7};
- int target = 9;
- int N = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {1,3,5,6,7};
+    int target;
 
- int result = binary_search(arr, 0, N-1, target);
+    printf("Enter an element to search: ");
+    scanf("%d", &target);
 
- if(result == -1)
- {
-  printf("Element not found.");
- }
- else
- {
-  printf("Element found.");
- }
- return 0;
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    int result = binary_search(arr, 0, N-1, target);
+
+    if(result == -1)
+    {
+        printf("Element not found.");
+    }
+    else
+    {
+        printf("Element found.");
+    }
+    return 0;
 }
